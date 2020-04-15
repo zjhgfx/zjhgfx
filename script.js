e1b574b9a98d0f4a9cbb2c80b680ca13b0b9eca8
zjhgfx <63724067+zjhgfx@users.noreply.github.com>
2020-04-15 15:55:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const text = document.querySelectorAll('.text');
+const halfX = window.innerWidth / 2;
+const halfY = window.innerHeight / 2;
+
+text.forEach((el, i) => {
+  TweenMax.to(el, 1, {
+    z: 1 * (i + 8) });
+
+});
+
+document.addEventListener('mousemove', e => {
+  text.forEach((el, i) => {
+    TweenMax.to(el, 0.5, {
+      x: (e.clientX - halfX) * (i + 1) * 0.01,
+      y: (e.clientY - halfY) * (i + 1) * 0.01 });
+
+  });
+});
